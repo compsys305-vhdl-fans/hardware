95d4294de6b8608603283057247093f2302da4f0
Parmjot Singh <parmjotsinghrobot@gmail.com>
2026-05-12 17:09:06 +1200

    Revert "TEMP: use old vga logic"
    This reverts commit cfe9924bf138b98394eac3f87b7bee7ad19405df.

--- src/vga.vhd
+++ src/vga.vhd
@@ -1,7 +1,5 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
-USE IEEE.STD_LOGIC_ARITH.ALL;
-USE IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 LIBRARY PROJECT_CONFIG;
 USE PROJECT_CONFIG.TYPES.ALL;
@@ -23,82 +21,70 @@ ENTITY vga IS
 END vga;
 
 ARCHITECTURE display OF vga IS
-    SIGNAL horiz_sync, vert_sync : STD_LOGIC;
-    SIGNAL video_on, video_on_v, video_on_h : STD_LOGIC;
-    SIGNAL h_count, v_count : STD_LOGIC_VECTOR(9 DOWNTO 0);
+    SIGNAL vga_s        : VGA_SCREEN;
 BEGIN
-    -- video_on is high only when RGB data is displayed
-    video_on <= video_on_h AND video_on_v;
+    -- video only when we are within the visible area
+    vga_s.video_on <= vga_s.video_on_h AND vga_s.video_on_v;
 
-    vga_controller: PROCESS
+    vga_controller: PROCESS (clock_25MHz)
+        VARIABLE req_on : STD_LOGIC;
     BEGIN
-        WAIT UNTIL (clock_25MHz'EVENT) AND (clock_25MHz = '1');
+        IF RISING_EDGE(clock_25MHz) THEN
+            -- horizontal sync
+            IF (vga_s.pixel_x = 799) THEN
+                vga_s.pixel_x <= 0;
+            ELSE
+                vga_s.pixel_x <= vga_s.pixel_x + 1;
+            END IF;
 
-        -- Generate Horizontal and Vertical Timing Signals for Video Signal
-        -- H_count counts pixels (640 + extra time for sync signals)
-        --
-        --  Horiz_sync  ------------------------------------__________--------
-        --  H_count       0                640             659       755    799
-        --
-        IF (h_count = 799) THEN
-            h_count <= "0000000000";
-        ELSE
-            h_count <= h_count + 1;
+            IF (vga_s.pixel_x <= 755) AND (vga_s.pixel_x >= 659) THEN
+                vga_s.hsync <= '0';
+            ELSE
+                vga_s.hsync <= '1';
+            END IF;
+
+            -- Match the known-good DE0-CV VGA_SYNC timing.
+            IF (vga_s.pixel_y >= 524) AND (vga_s.pixel_x >= 699) THEN
+                vga_s.pixel_y <= 0;
+            ELSIF (vga_s.pixel_x = 699) THEN
+                vga_s.pixel_y <= vga_s.pixel_y + 1;
+            END IF;
+
+            IF (vga_s.pixel_y <= 494) AND (vga_s.pixel_y >= 493) THEN
+                vga_s.vsync <= '0';
+            ELSE
+                vga_s.vsync <= '1';
+            END IF;
+
+            -- generate video on/off signals
+            IF (vga_s.pixel_x <= 639) THEN
+                vga_s.video_on_h <= '1';
+                screen.pixel_x <= vga_s.pixel_x;
+            ELSE
+                vga_s.video_on_h <= '0';
+            END IF;
+
+            IF (vga_s.pixel_y <= 479) THEN
+                vga_s.video_on_v <= '1';
+                screen.pixel_y <= vga_s.pixel_y;
+            ELSE
+                vga_s.video_on_v <= '0';
+            END IF;
+
+            -- Whether the *requested* pixel position is within the visible screen.
+            IF (vga_s.pixel_x <= 639) AND (vga_s.pixel_y <= 479) THEN
+                req_on := '1';
+            ELSE
+                req_on := '0';
+            END IF;
+            in_screen <= req_on;
+
+            -- drive display outputs
+            r_out <= r_in AND vga_s.video_on;
+            g_out <= g_in AND vga_s.video_on;
+            b_out <= b_in AND vga_s.video_on;
+            hsync <= vga_s.hsync;
+            vsync <= vga_s.vsync;
         END IF;
-
-        -- Generate Horizontal Sync Signal using H_count
-        IF (h_count <= 755) AND (h_count >= 659) THEN
-            horiz_sync <= '0';
-        ELSE
-            horiz_sync <= '1';
-        END IF;
-
-        -- V_count counts rows of pixels (480 + extra time for sync signals)
-        --
-        --  Vert_sync      -----------------------------------------------_______------------
-        --  V_count         0                                      480    493-494          524
-        --
-        IF (v_count >= 524) AND (h_count >= 699) THEN
-            v_count <= "0000000000";
-        ELSIF (h_count = 699) THEN
-            v_count <= v_count + 1;
-        END IF;
-
-        -- Generate Vertical Sync Signal using V_count
-        IF (v_count <= 494) AND (v_count >= 493) THEN
-            vert_sync <= '0';
-        ELSE
-            vert_sync <= '1';
-        END IF;
-
-        -- Generate Video on Screen Signals for Pixel Data
-        IF (h_count <= 639) THEN
-            video_on_h <= '1';
-            screen.pixel_x <= CONV_INTEGER(h_count);
-        ELSE
-            video_on_h <= '0';
-        END IF;
-
-        IF (v_count <= 479) THEN
-            video_on_v <= '1';
-            screen.pixel_y <= CONV_INTEGER(v_count);
-        ELSE
-            video_on_v <= '0';
-        END IF;
-
-        -- Generate in_screen signal for *requested* pixel position
-        IF (h_count <= 639) AND (v_count <= 479) THEN
-            in_screen <= '1';
-        ELSE
-            in_screen <= '0';
-        END IF;
-
-        -- Put all video signals through registers to improve timing
-        r_out <= r_in AND video_on;
-        g_out <= g_in AND video_on;
-        b_out <= b_in AND video_on;
-        hsync <= horiz_sync;
-        vsync <= vert_sync;
-
     END PROCESS vga_controller;
 END display;
